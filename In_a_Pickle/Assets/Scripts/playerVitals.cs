8a3d87d03dca2d6531b8ef3c5909f7ff1cbd2bc1
Kyle Gulliver <kgulliver@mun.ca>
2020-10-20 21:02:00 -0230

    Pickle Moistnesss

---
+++ In_a_Pickle/Assets/Scripts/playerVitals.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerVitals : MonoBehaviour
+{
+    //public Slider staminaSlider;
+    //public int staminaFallRate;
+    //public int maxStamina;
+
+
+    public Slider hydrationSlider;
+    public int maxHydration;
+    public int hydrationFallRate;
+
+
+    void Start()
+    {
+        //staminaSlider.maxValue = maxStamina;
+        //staminaSlider.value = maxStamina;
+        
+        hydrationSlider.maxValue = maxHydration;
+        hydrationSlider.value = maxHydration;
+
+    }
+
+    void Update()
+    {
+
+        //Hydration
+        if(hydrationSlider.value >= 0)
+        {
+            hydrationSlider.value -= Time.deltaTime / hydrationFallRate;
+        }
+
+        else if(hydrationSlider.value <= 0)
+        {
+            hydrationSlider.value = 0;
+        }
+
+        else if(hydrationSlider.value >= maxHydration)
+        {
+            hydrationSlider.value = maxHydration;
+        }
+
+        if(hydrationSlider.value == 0)
+        {
+            CharacterDeath();
+            
+
+            
+        }
+    }
+
+    private void CharacterDeath()
+    {
+        Debug.Log("dead");
+    }
+}
